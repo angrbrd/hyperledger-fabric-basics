425db1ce53e828b6ec9d1164c1119995210a2d31
Mr. Angry <adderbak@us.ibm.com>
2016-10-22 23:46:23 -0400

    Restructuring Application Code
    Application now performs required setup, user registration and
    enrollment, and chaincode deployment. The invocation and query
    functions are not triggered automatically.
    
    Signed-off-by: Anna D Derbakova <adderbak@us.ibm.com>

--- crowd_fund_demo.js
+++ crowd_fund_demo.js
@@ -1,12 +1,16 @@
 var hfc = require('hfc');
-var test = require('tape');
 var util = require('util');
 
 //
 // Get the Docker Host IP from command line
 //
 var DOCKER_HOST_IP = process.argv[2];
-console.log("Docker Host IP: " + DOCKER_HOST_IP + "\n");
+if (DOCKER_HOST_IP == null || DOCKER_HOST_IP == "") {
+	console.log("ERROR: No Docker Host IP specified! Exiting.");
+	process.exit(1);
+} else {
+	console.log("Docker Host IP: " + DOCKER_HOST_IP + "\n");
+}
 
 //
 // Required configuration settings
@@ -22,7 +26,7 @@ var SDK_PEER_ADDRESSES = [
 var SDK_EVENTHUB_ADDRESS = "grpc://" + DOCKER_HOST_IP + ":7053";
 
 //
-//  Create a test chain object
+//  Create a chain object
 //
 var chain = hfc.newChain("testChain");
 
@@ -48,7 +52,8 @@ if (process.argv[3] == "single-peer") {
 		chain.addPeer(peer_address);
 	});
 } else {
-	console.log("ERROR: Please select either a `single-peer` or a `four-peer` network!");
+	console.log("ERROR: Please select either a `single-peer` " +
+	 			" or a `four-peer` network!");
 	process.exit(1);
 }
 
@@ -56,96 +61,101 @@ if (process.argv[3] == "single-peer") {
 console.log("Setting eventHubAddr address to: " + SDK_EVENTHUB_ADDRESS + "\n");
 chain.eventHubConnect(SDK_EVENTHUB_ADDRESS);
 process.on('exit', function () {
-  chain.eventHubDisconnect();
+	console.log("Exiting and disconnecting eventHub channel.");
+	chain.eventHubDisconnect();
 });
 
-// Set the chaincode deployment mode to network, i.e. chaincode runs inside
+// Set the chaincode deployment mode to "network", i.e. chaincode runs inside
 // a Docker container
 chain.setDevMode(false);
 
 //
-// Declare test variables that will be used across multiple tests
+// Declare variables that will be used across multiple operations
 //
 
-// Member object returned after registration and enrollment
-var test_user_Member1;
+// User object returned after registration and enrollment
+var app_user;
 
-// testChaincodeID will store the chaincode ID value after deployment and used
-// to execute the invocation and query
-var testChaincodeID;
+// chaincodeID will store the chaincode ID value after deployment which is
+// later used to execute invocations and queries
+var chaincodeID;
 
 //
 // Enroll the WebAppAdmin member. WebAppAdmin member is already registered
-// manually by being included inside the membersrvc.yaml file.
+// manually by being included inside the membersrvc.yaml file, i.e. the
+// configuration file for the membership services Docker container.
 //
+chain.getMember("WebAppAdmin", function (err, WebAppAdmin) {
+    if (err) {
+		console.log("ERROR: Failed to get WebAppAdmin member -- " + err);
+		process.exit(1);
+    } else {
+        console.log("Successfully got WebAppAdmin member.");
 
-test('Enroll WebAppAdmin', function (t) {
-    // Get the WebAppAdmin member
-    chain.getMember("WebAppAdmin", function (err, WebAppAdmin) {
-        if (err) {
-            t.fail("Failed to get WebAppAdmin member " + " ---> " + err);
-            t.end(err);
-        } else {
-            t.pass("Successfully got WebAppAdmin member" /*+ " ---> " + JSON.stringify(crypto)*/);
+        // Enroll the WebAppAdmin member with the certificate authority using
+        // the one time password hard coded inside the membersrvc.yaml.
+        pw = "DJY27pEnl16d";
+        WebAppAdmin.enroll(pw, function (err, enrollment) {
+            if (err) {
+                console.log("ERROR: Failed to enroll WebAppAdmin member -- "
+							+ err);
+                process.exit(1);
+            } else {
+				// Set the WebAppAdmin as the designated chain registrar
+				console.log("Successfully enrolled WebAppAdmin member.");
+				console.log("Setting WebAppAdmin as chain registrar.");
+				chain.setRegistrar(WebAppAdmin);
 
-            // Enroll the WebAppAdmin member with the certificate authority using
-            // the one time password hard coded inside the membersrvc.yaml.
-            pw = "DJY27pEnl16d";
-            WebAppAdmin.enroll(pw, function (err, enrollment) {
-                if (err) {
-                    t.fail("Failed to enroll WebAppAdmin member " + " ---> " + err);
-                    t.end(err);
-                } else {
-					// Set the WebAppAdmin as the designated chain registrar
-					chain.setRegistrar(WebAppAdmin);
-
-                    t.pass("Successfully enrolled WebAppAdmin member");
-					t.end(err);
-                }
-            });
-        }
-    });
-});  /* Enroll WebAppAdmin */
+				// Register a new user with WebAppAdmin as the chain registrar
+				console.log("Registering user `WebAppUser_1`.");
+				registerUser("WebApp_user1");
+            }
+        });
+    }
+});
 
 //
 // Register and enroll a new user with the certificate authority.
-// This will be performed by the registrar member, WebAppAdmin.
+// This will be performed by the member with registrar authority, WebAppAdmin.
 //
-
-test('Register and enroll a new user', function (t) {
-    // Register and enroll test_user
-    chain.getMember("WebApp_user1", function (err, WebApp_user1) {
+function registerUser(user_name) {
+    // Register and enroll the user
+    chain.getMember(user_name, function (err, user) {
         if (err) {
-            t.fail("Failed to get WebApp_user1"  + " ---> ", err);
-			t.end(err);
+            console.log("ERROR: Failed to get " + user.getName() + " -- ", err);
+			process.exit(1);
         } else {
-            test_user_Member1 = WebApp_user1;
+            app_user = user;
 
-			// User may not be enrolled yet. Perform both registration and enrollment.
+			// User may not be enrolled yet. Perform both registration
+			// and enrollment.
 			var registrationRequest = {
-				enrollmentID: test_user_Member1.getName(),
+				enrollmentID: app_user.getName(),
 				affiliation: "bank_a"
 			};
-			test_user_Member1.registerAndEnroll(registrationRequest, function (err, member) {
+			app_user.registerAndEnroll(registrationRequest, function (err, member) {
 				if (err) {
-					t.fail("Failed to enroll WebApp_user1 member " + " ---> " + err);
-					t.end(err);
+					console.log("ERROR: Failed to enroll "
+								+ app_user.getName() + " -- " + err);
+					process.exit(1);
 				} else{
-		            t.pass("Successfully registered and enrolled " + test_user_Member1.getName());
-					t.end();
+		            console.log("Successfully registered and enrolled "
+								+ app_user.getName() + ".\n");
+
+					// Deploy a chaincode with the new user
+					console.log("Deploying chaincode now...");
+					deployChaincode()
 				}
 			});
         }
     });
-}); /* Register and enroll a new user */
+}
 
 //
-// Create and issue a chaincode deploy request by the test user, who was
-// registered and enrolled in the UT above. Deploy a testing chaincode from
+// Construct and issue a chaincode deployment request. Deploy the chaincode from
 // a local directory in the user's $GOPATH.
 //
-
-test('Deploy a chaincode by enrolled user', function (t) {
+function deployChaincode() {
     // Construct the deploy request
     var deployRequest = {
 		// Path (under $GOPATH/src) required for deploy in network mode
@@ -157,36 +167,31 @@ test('Deploy a chaincode by enrolled user', function (t) {
     };
 
     // Trigger the deploy transaction
-    var deployTx = test_user_Member1.deploy(deployRequest);
+    var deployTx = app_user.deploy(deployRequest);
 
-    // Print the deploy results
+    // Print the successfull deploy results
     deployTx.on('complete', function (results) {
-        // Deploy request completed successfully
-        console.log(util.format("deploy results: %j", results));
-        // Set the testChaincodeID for subsequent tests
-        testChaincodeID = results.chaincodeID;
-        console.log("testChaincodeID:" + testChaincodeID);
-        t.pass(util.format("Successfully deployed chaincode: request=%j, response=%j", deployRequest, results));
-		t.end();
+        // Set the chaincodeID for subsequent tests
+        chaincodeID = results.chaincodeID;
+        console.log(util.format("Successfully deployed chaincode: request=%j, "
+					+ "response=%j" + "\n", deployRequest, results));
     });
     deployTx.on('error', function (err) {
         // Deploy request failed
-        t.fail(util.format("Failed to deploy chaincode: request=%j, error=%j", deployRequest, err));
-		t.end();
+        console.log(util.format("ERROR: Failed to deploy chaincode: request=%j, "
+					+ "error=%j", deployRequest, err));
+		process.exit(1);
     });
-}); /* Deploy a chaincode by enrolled user */
+}
 
 //
-// Create and issue a chaincode query request by the test user, who was
-// registered and enrolled in the UT above. Query an existing chaincode
-// state variable with a transaction certificate batch size of 1.
+// Create and issue a chaincode query request.
 //
-
-test('Query existing chaincode state by enrolled user with batch size of 1', function (t) {
+function queryChaincode() {
     // Construct the query request
     var queryRequest = {
         // Name (hash) required for query
-        chaincodeID: testChaincodeID,
+        chaincodeID: chaincodeID,
         // Function to trigger
         fcn: "query",
         // Existing state variable to retrieve
@@ -194,32 +199,32 @@ test('Query existing chaincode state by enrolled user with batch size of 1', fun
     };
 
     // Trigger the query transaction
-    test_user_Member1.setTCertBatchSize(1);
-    var queryTx = test_user_Member1.query(queryRequest);
+    app_user.setTCertBatchSize(1);
+    var queryTx = app_user.query(queryRequest);
 
     // Print the query results
     queryTx.on('complete', function (results) {
         // Query completed successfully
-        t.pass(util.format("Successfully queried existing chaincode state: request=%j, response=%j, value=%s", queryRequest, results, results.result.toString()));
-		t.end();
+        console.log(util.format("Successfully queried existing chaincode state: "
+					+ "request=%j, response=%j, value=%s", queryRequest, results,
+					results.result.toString()));
 	});
     queryTx.on('error', function (err) {
         // Query failed
-        t.fail(util.format("Failed to query existing chaincode state: request=%j, error=%j", queryRequest, err));
-		t.end();
+        console.log(util.format("ERROR: Failed to query existing chaincode " +
+					+ "state: request=%j, error=%j", queryRequest, err));
+		process.exit(1);
 	});
-}); /* Query existing chaincode state by enrolled user with batch size of 1 */
+}
 
 //
-// Create and issue a chaincode invoke request by the test user, who was
-// registered and enrolled in the UT above.
+// Create and issue a chaincode invoke request.
 //
-
-test('Invoke a chaincode by enrolled user', function (t) {
+function invokeChaincode() {
 	// Construct the invoke request
     var invokeRequest = {
         // Name (hash) required for invoke
-        chaincodeID: testChaincodeID,
+        chaincodeID: chaincodeID,
         // Function to trigger
         fcn: "invoke",
         // Parameters for the invoke function
@@ -227,18 +232,18 @@ test('Invoke a chaincode by enrolled user', function (t) {
     };
 
     // Trigger the invoke transaction
-    var invokeTx = test_user_Member1.invoke(invokeRequest);
+    var invokeTx = app_user.invoke(invokeRequest);
 
     // Print the invoke results
     invokeTx.on('submitted', function (results) {
         // Invoke transaction submitted successfully
-        t.pass(util.format("Successfully submitted chaincode invoke transaction: request=%j, response=%j", invokeRequest, results));
-        chain.eventHubDisconnect();
-		t.end();
+        console.log(util.format("Successfully submitted chaincode invoke " +
+					" transaction: request=%j, response=%j", invokeRequest, results));
     });
     invokeTx.on('error', function (err) {
         // Invoke transaction submission failed
-        t.fail(util.format("Failed to submit chaincode invoke transaction: request=%j, error=%j", invokeRequest, err));
-		t.end();
+        console.log(util.format("ERROR: Failed to submit chaincode invoke "
+					+ "transaction: request=%j, error=%j", invokeRequest, err));
+		process.exit(1);
 	});
-}); /* Invoke a chaincode by enrolled user */
+}
