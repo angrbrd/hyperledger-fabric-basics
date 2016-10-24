88e2132e9a466f16931640aac2194cf37f46ec74
Mr. Angry <adderbak@us.ibm.com>
2016-10-23 21:45:57 -0400

    Renaming The Chaincode File
    Renaming the chaincode folder and file to crowd_fund_chaincode.
    
    Signed-off-by: Anna D Derbakova <adderbak@us.ibm.com>

--- crowd_fund_demo.js
+++ crowd_fund_demo.js
@@ -183,7 +183,7 @@ function deployChaincode() {
     // Construct the deploy request
     var deployRequest = {
 		// Path (under $GOPATH/src) required for deploy in network mode
-		chaincodePath: "chaincode_example02",
+		chaincodePath: "crowd_fund_chaincode",
         // Function to trigger
         fcn: "init",
         // Arguments to the initializing function
